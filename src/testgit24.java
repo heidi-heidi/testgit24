92b622e71ebf245eb872ef23aa562ce932866f81
zhouren <624162505@qq.com>
2021-02-28 21:50:00 +0800

    commit develop

--- src/testgit24.java
+++ src/testgit24.java
@@ -1,6 +1,8 @@
 public class testgit24 {
     public static void main(String[] args) {
         System.out.println("testgit24");
+        System.out.println("testgit24develop");
+
         //4
         //5
         //61
