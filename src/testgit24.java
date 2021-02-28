f41e625e72323b5d7a0c533c97feacd1b0d02b8a
zhouren <624162505@qq.com>
2021-02-28 21:36:16 +0800

    Revert "62commit"
    This reverts commit d4101a200fa5317acfcc5978cd01f4666fbdc410.

--- src/testgit24.java
+++ src/testgit24.java
@@ -1,12 +1,9 @@
 public class testgit24 {
     public static void main(String[] args) {
         System.out.println("testgit24");
-        System.out.println("testgit24R1");
-
         //4
         //5
         //61
-        //62
 
     }
 
